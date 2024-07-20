69762013615d11e182360456d5c9abf532b1651f
ShreyP. <shreyprajapati13@gmail.com>
2024-07-20 18:57:39 +0530

    commit-2

---
+++ script.js
@@ -0,0 +1,63 @@
+const APIKEY = `api_key=789ecf8082011b15f21787dac77dc8ee`;
+const baseURL = `https://api.themoviedb.org/3`;
+const imageURL = `https://image.tmdb.org/t/p/w500`;
+
+const requests = {
+  fetchPopular: `${baseURL}/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&${APIKEY}`,
+  fetchTrending: `${baseURL}/trending/all/week?${APIKEY}&language=en-US`,
+  // fetchNetflixOrignals: `${baseURL}/discover/tv?${APIKEY}&with_networks=213`,
+  fetchActionMovies: `${baseURL}/discover/movie?${APIKEY}&with_genres=28`,
+  fetchComedyMovies: `${baseURL}/discover/movie?${APIKEY}&with_genres=35`,
+  fetchHorrorMovies: `${baseURL}/discover/movie?${APIKEY}&with_genres=27`,
+  fetchRomanceMovies: `${baseURL}/discover/movie?${APIKEY}&with_genres=10749`,
+  fetchDocumentaries: `${baseURL}/discover/movie?${APIKEY}&with_genres=99`,
+};
+
+const categories = [
+  "populer",
+  "trending",
+  // "originals",
+  "action",
+  "comedy",
+  "horror",
+  "romance",
+  "documentries",
+];
+
+async function fetchCheck(url, className) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    appendCards(data, className);
+  } catch (error) {
+    alert("Failed to load images");
+  }
+}
+
+function appendCards(data, className) {
+  data.results.forEach((obj) => {
+    if (obj.title !== undefined) { 
+      const newCard = document.createElement("div");
+      const cardImage = document.createElement('img');
+      const title = document.createElement('p');
+      cardImage.src = `${imageURL}/${obj.poster_path}`;
+      title.innerHTML = `${obj.title}`;
+      cardImage.className = 'cardimage';
+      newCard.appendChild(cardImage);
+      newCard.appendChild(title);
+      newCard.className = "card";
+      document.querySelector(`.${className}`).appendChild(newCard);
+    }
+  });
+}
+
+function apiHandler() {
+  let i = 0;
+
+  for (const key in requests) {
+    fetchCheck(requests[key], categories[i]);
+    i++;
+  }
+}
+
+apiHandler();
